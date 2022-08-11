4b501517aae5d2363fad00f4b01a610400bf51a9
USERMIC-9H64N6C\BIFF2020 <dnjswnsthd@naver.com>
2022-08-11 11:28:46 +0900

    Add front06_Ajax

---
+++ front06_Ajax/src/servlet/ajax/HelloAjax.java
@@ -0,0 +1,37 @@
+package servlet.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloAjax")
+public class HelloAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+		String data = request.getParameter("data");
+		out.print(data);
+	}
+}
